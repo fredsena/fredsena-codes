cde2f882194e81ee92da9cf8b8152faf3c9c6536
Fred Sena <sena.fred@gmail.com>
2015-05-12 10:38:55 -0300

    Update c#_codes_stuff.cs

--- c#_codes_stuff.cs
+++ c#_codes_stuff.cs
@@ -1,48 +1,13 @@
-Enter file contents here
-
-Linq NOT IN:
-
-public JsonResult GetCursos(string alunoId)
-{
-    int id = Convert.ToInt32(alunoId);
-    var cursosNotIn = db.AlunoCurso.Where(x => x.AlunoId == id).Select(y => y.CursoId);
-    var result = db.Curso.Where(x => !cursosNotIn.Contains(x.CursoId)).Select(c => new CursoModel { CursoId = c.CursoId, NomeCurso = c.NomeCurso }).ToList();
-    return Json(result);
-}
 
 Password=usr;Persist Security Info=False;User ID=Usr;Initial Catalog=Bd;Data Source=BD
 
 Data Source=.\SQLEXPRESS;Initial Catalog=MvcMusicStore;Integrated Security=True
 
-//Join list of string to comma separated and enclosed in single quotes
-var dados = dt.Rows.Cast<DataRow>().Where(x => x.Field<string>("Tem") == "1");
-
-string planos = "'" + String.Join("','", dados.Select(row => row.Field<string>("Plano").ToString()).ToArray()) + "'";
-
-var countReceita = (from r in dt.AsEnumerable()
-					where r.Field<string>("DscTipo").Trim().Equals("campo")
-					select r).Count();
-					
-var sum = table.AsEnumerable().Sum(x=>x.Field<int>(3));
-var sum = table.AsEnumerable().Sum(x=>x.Field<int>("SomeProperty"));
-					
-
-//Data table to array int 
-var result = dt.Rows.Cast<DataRow>().Select(row => row[0].ToString()).ToArray();
-int[] dr2 = Array.ConvertAll(result, (p => Convert.ToInt32(p)));
-
-//Seperado por Virgula
-String.Split
-
+//splited by comma
 var ramos = String.Join(",", Lista.Select(l => l.Desc).ToArray());
 
 //DATETIME NULL
 return dt.Rows[0]["DatRgsTemLib"] != DBNull.Value ? Convert.ToDateTime(dt.Rows[0].ToString()) : (DateTime?) null;
-
-DataRow[] dr = dt.AsEnumerable().Take(1).ToArray();
-object[] dr1 = dr[0].ItemArray;
-int[] dr2 = Array.ConvertAll(dr1, (p => Convert.ToInt32(p)));
-
 
 public DataTable BuscaDestinatario(string usuarioId)
   {

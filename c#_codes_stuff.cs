99662d9729a6c142cbcfbba9f7bf19436db83996
Fred Sena <sena.fred@gmail.com>
2016-05-16 10:55:53 -0300

    Update c#_codes_stuff.cs

--- c#_codes_stuff.cs
+++ c#_codes_stuff.cs
@@ -1,19 +1,27 @@
 
+/* *******************************************************************/
+//Get the name of current function
+Console.WriteLine(new StackTrace().GetFrame(0).GetMethod().Name.ToString());
+/* *******************************************************************/
+
 //Get project path: COOL!!!!!!
 string projectPath = Path.GetDirectoryName(Path.GetDirectoryName(System.IO.Directory.GetCurrentDirectory()));
+/* *******************************************************************/
 
 string connString = ConfigurationManager.ConnectionStrings["Connection"].ConnectionString;
-
 Password=usr;Persist Security Info=False;User ID=Usr;Initial Catalog=Bd;Data Source=BD
-
 Data Source=.\SQLEXPRESS;Initial Catalog=MvcMusicStore;Integrated Security=True
+/* *******************************************************************/
 
 //splited by comma
 var ramos = String.Join(",", Lista.Select(l => l.Desc).ToArray());
+/* *******************************************************************/
 
 //DATETIME NULL
 return dt.Rows[0]["DatRgsTemLib"] != DBNull.Value ? Convert.ToDateTime(dt.Rows[0].ToString()) : (DateTime?) null;
+/* *******************************************************************/
 
+//Connection and query
 public DataTable BuscaDestinatario(string usuarioId)
   {
       DataTable dt = new DataTable();

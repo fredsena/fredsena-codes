7feec07d95350093eed6a21057b5de5a7af5365b
Fred Sena <sena.fred@gmail.com>
2016-08-24 17:51:40 -0300

    Update DOS.cs

--- DOS.cs
+++ DOS.cs
@@ -1,3 +1,9 @@
 
 //Merge a text (.txt) file in the Windows command line
 copy *.txt newfile.txt
+
+//Recursively copy files of a specific pattern into a single flat file on Windows
+mkdir targetDir
+for /r %x in (*.cs) do copy "%x" targetDir\ /Y
+cd targetDir
+copy *.cs newfile.txt

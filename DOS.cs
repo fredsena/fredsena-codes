7b67171b9bc4e1d34c20297c7242b8fd55a32f2e
Fred Sena <sena.fred@gmail.com>
2017-03-20 10:38:04 -0300

    Update DOS.cs

--- DOS.cs
+++ DOS.cs
@@ -7,3 +7,18 @@ mkdir targetDir
 for /r %x in (*.cs) do copy "%x" targetDir\ /Y
 cd targetDir
 copy *.cs newfile.txt
+
+//Remove Connected user in Remote Desktop Connection
+//https://superuser.com/questions/585922/remove-connected-user-in-remote-desktop-connection
+mstsc /v:n.n.n.n /admin
+
+/*
+To connect to the Terminal Server despite the limited connections, what you can do is to connect to the server with the /admin switch. To do this, launch mstsc as follows:
+
+mstsc /v:n.n.n.n /admin
+Replace n.n.n.n with the IP of the server and you’re good to go. Once connected to the server, use Task Manager to log off the sessions that are no longer used.
+
+To do this, select the session you wish to disconnect and click Logoff. Note that this will effectively log off the session and closes all windows still open in that session!
+
+Once these steps have been completed, log your current session off and you should be able to reconnect to the server without using the /admin switch.
+*/
